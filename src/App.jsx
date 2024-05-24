176f009fdcc09e658fcda41c15c90bcfd06f0855
abir2010 <kaiserabir222@gmail.com>
2024-05-25 00:01:59 +0600

    project initial setup complete

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen text-3xl font-bold font-mono">
+      Welcome To Vivid Store
+    </div>
+  );
+}
+
+export default App;
